2567b37c3d7254320e6b1b2ec79d4bf075d4c306
TzuchiLin <kaylin7938@gmail.com>
2017-08-08 11:32:45 +0800

    first commit

---
+++ 01/TzuchiLin/assignment_1_1.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+import random
+print("猜数字游戏开始了！")
+#random.randint(a, b),n: a <= n <= b
+random_num = random.randint(0,100)
+
+i=0
+while i<5:
+	input_num = input("請猜一个数字，范围为1~100：")
+	input_num = int(input_num)
+	if input_num < random_num:
+		print("猜小了")
+	elif input_num > random_num:
+		print("猜大了")
+	else:
+		print("恭喜！猜对了！") 
+		break
+	i+=1		
+if i==5:
+	print("太笨了，下次再来！")
+
+
+
